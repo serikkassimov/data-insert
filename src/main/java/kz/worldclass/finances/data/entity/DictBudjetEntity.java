8cfde3325b5f370b4d0280964d8b0be1b48d832b
Serik Kassimov <serik.kasimov@gmeil.com>
2017-10-07 09:20:59 +0600

    Dict_budjet добавил

---
+++ src/main/java/kz/worldclass/finances/data/entity/DictBudjetEntity.java
@@ -0,0 +1,42 @@
+package kz.worldclass.finances.data.entity;
+
+import kz.worldclass.finances.data.entity.base.BaseDictEntity;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+import static kz.worldclass.finances.data.entity.DictRoleEntity.*;
+
+@Entity
+@Table(
+        schema = SCHEMA, name = TABLE,
+        indexes = {
+            @Index(
+                    name = IND + SEP + TABLE + SEP + COL_CODE,
+                    unique = true,
+                    columnList = COL_CODE
+            )
+        },
+        uniqueConstraints = {
+            @UniqueConstraint(
+                    name = UNQ + SEP + TABLE + SEP + COL_CODE,
+                    columnNames = {COL_CODE}
+            )
+        }
+)
+@SequenceGenerator(name = SEQ, schema = SCHEMA, sequenceName = SEQ + SEP + TABLE, allocationSize = 10)
+@SuppressWarnings("PersistenceUnitPresent")
+public class DictBudjetEntity extends BaseDictEntity {
+    public static final String TABLE = "DICT_BUDJET";
+    
+    @OneToMany(mappedBy = UserRoleLinkEntity.PROP_ROLE)
+    private Collection<UserRoleLinkEntity> userLinks;
+
+    public Collection<UserRoleLinkEntity> getUserLinks() {
+        return userLinks;
+    }
+
+    public void setUserLinks(Collection<UserRoleLinkEntity> userLinks) {
+        this.userLinks = userLinks;
+    }
+}
