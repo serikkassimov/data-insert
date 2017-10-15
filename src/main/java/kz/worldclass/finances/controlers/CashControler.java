410e57a55155730bc8f427822439295cef3e54cb
Serik Kassimov <serik.kasimov@gmeil.com>
2017-10-15 07:18:23 +0600

    Заявка

---
+++ src/main/java/kz/worldclass/finances/controlers/CashControler.java
@@ -0,0 +1,33 @@
+package kz.worldclass.finances.controlers;
+
+
+import kz.worldclass.finances.data.entity.EntityFilials;
+import kz.worldclass.finances.data.entity.EntityIndicators;
+import kz.worldclass.finances.services.DataService;
+import kz.worldclass.finances.services.FilialService;
+import kz.worldclass.finances.services.IndicatorSevice;
+import kz.worldclass.finances.services.UtilService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ParseException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * Created by serik on 22.03.17.
+ */
+
+//@RestController
+//@ComponentScan({"kz.worldclass.finances.services"})
+public class CashControler {
+
+    @RequestMapping(value = {"/incomes"}, method = RequestMethod.GET)
+    public ModelAndView incomes(HttpServletResponse response) throws ParseException {
+        ModelAndView res = new ModelAndView("incomes");
+        res.addObject("filial", 1);
+        return res;
+    }
+
+}
--- src/main/webapp/pages/components/page-views/request-report.vue
+++ src/main/webapp/pages/components/page-views/request-report.vue
@@ -5,19 +5,135 @@
             <el-row>
                 <h1 class="h1">{{ text }}</h1>
             </el-row>
-            <el-row>
-                <el-button type="success" @click="saveData">Сохранить</el-button>
-            </el-row>
+
             <el-row>
                 <el-form>
                     <el-form-item label="Отчетная дата">
-                        <el-date-picker type="date" placeholder="Отчетная дата" v-model="formData.date"
+                        <el-date-picker type="date" placeholder="Дата платежа" v-model="formData.date"
                                         :format="'dd.MM.yyyy'"></el-date-picker>
                     </el-form-item>
                 </el-form>
             </el-row>
-
+            <el-row>
+                <el-button type="success" @click="addData">Новая заявка</el-button>
+            </el-row>
+            <el-form>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Номер</th>
+                        <th>Вид</th>
+                        <th>Статья бюджета</th>
+                        <th>Назначение</th>
+                        <th>Сумма</th>
+                        <th>Действие</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(item, index) in formData.values">
+                            <td>{{index+1}}</td>
+                            <td>
+                                <el-select v-model="item.store_type" placeholder="Выберите тип">
+                                    <el-option v-for="item in dict_fin_store_type" :label="item.name"
+                                               :value="item.code"></el-option>
+                                </el-select>
+                            </td>
+                            <td>
+                                <el-select v-model="item.budget" placeholder="Выберите статью бюджета">
+                                    <el-option v-for="item in dict_budget" :label="item.name" :value="item.code"></el-option>
+                                </el-select>
+                            </td>
+                            <td>
+                                <el-input v-model="item.note" placeholder="Назначение текст"></el-input>
+                            </td>
+                            <td>
+                                <el-input-number v-model="item.sum" placeholder="Сумма"></el-input-number>
+                            </td>
+                            <td>
+                                <el-button @click="deleteRow(index)">Удалить</el-button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <!--
+                <el-table
+                        :data="formData.values"
+                        border
+                        style="width: 100%"
+                        max-height="250">
+                    <el-table-column
+                            type="index"
+                            label="Номер"
+                            width="100">
+                    </el-table-column>
+                    <el-table-column
+                            prop="store_type"
+                            label="Вид"
+                            width="150">
+                    </el-table-column>
+                    <el-table-column
+                            prop="note"
+                            label="Назначение">
+                    </el-table-column>
+                    <el-table-column
+                            prop="budget"
+                            label="Статья бюджета"
+                            width="350">
+                    </el-table-column>
+                    <el-table-column
+                            prop="sum"
+                            label="Сумма"
+                            width="150">
+                    </el-table-column>
+                    <el-table-column
+                            label="Operations"
+                            width="120">
+                        <template scope="scope">
+                            <el-button
+                                    @click.native.prevent="editRow(scope.$index, formData.values)"
+                                    type="text"
+                                    size="small">
+                                Изменить
+                            </el-button>
+                            <el-button
+                                    @click.native.prevent="deleteRow(scope.$index, formData.values)"
+                                    type="text"
+                                    size="small">
+                                Удалить
+                            </el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+                -->
+            </el-form>
         </div>
+       <!--
+        <el-dialog title="Ввод данных по одной заявке" :visible.sync="dialogFormVisible">
+            <el-form :model="form" label-width="150px">
+                <el-form-item label="Статья бюджета">
+                    <el-select v-model="form.budget" placeholder="Выберите статью бюджета">
+                        <el-option v-for="item in dict_budget" :label="item.name" :value="item.code"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="Вид">
+                    <el-select v-model="form.store_type" placeholder="Выберите тип">
+                        <el-option v-for="item in dict_fin_store_type" :label="item.name"
+                                   :value="item.code"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="Назначение">
+                    <el-input v-model="form.note" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="Сумма">
+                    <el-input-number v-model="form.sum" auto-complete="off"></el-input-number>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogFormVisible = false">Отмена</el-button>
+    <el-button type="primary" @click="saveDialogForm">Сохранить</el-button>
+  </span>
+        </el-dialog>
+-->
         <div>
             <router-view></router-view>
         </div>
@@ -47,11 +163,38 @@
             template: '#' + componentName,
             data: function () {
                 return {
+                    isNewData: false,
+                    dialogFormVisible: false,
+                    form: {
+                        budget: "",
+                        store_type: "",
+                        note: "",
+                        sum: 0,
+                    },
                     text: 'Заявка',
                     formData: {
                         date: "",
                         values: []
-                    }
+                    },
+                    dict_budget: [
+                        {
+                            code: 1000,
+                            name: "Аренда",
+                        },
+                        {
+                            code: "2000",
+                            name: "Содержание объектов"
+                        }
+                    ],
+                    dict_fin_store_type: [
+                        {
+                            code: 1,
+                            name: "Наличные"
+                        }, {
+                            code: 2,
+                            name: "Безналичные"
+                        }
+                    ]
                 };
             },
             computed: Vuex.mapState({
@@ -62,8 +205,44 @@
                 b: state => 'b'
             }),
             methods: {
-                saveData : function () {
-                    console.log("test")
+                addData: function () {
+                   /* this.form = {};
+                    this.isNewData = true;
+                    this.dialogFormVisible = true;
+                    */
+                   var newRow = {
+                       store_type: "",
+                       budget: "",
+                       note: "",
+                       sum: 0,
+                   }
+                   this.formData.values.push(newRow);
+                },
+                deleteRow: function(rowId) {
+                    console.log("data", this.formData.values)
+                    this.formData.values.splice(rowId, 1)
+                },
+                loadBuget: function () {
+
+                },
+                saveDialogForm: function () {
+                    if (this.isNewData) {
+                        var nf = {};
+                        var that = this;
+                        for (var prop in that.form) {
+                            if (that.form.hasOwnProperty(prop)) {
+                                nf[prop] = that.form[prop];
+                            }
+                        }
+                        this.formData.values.push(nf);
+                    }
+                    this.dialogFormVisible = false;
+                },
+
+                editRow(index, rows) {
+                    this.isNewData = false;
+                    this.form = rows[index];
+                    this.dialogFormVisible = true;
                 }
             }
         });
